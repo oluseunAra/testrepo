7a4de2e975513abd90b43fee5a44e1581a5a63c7
OluseunAra Olarinde <oluseun.ola@gmail.com>
2018-01-19 13:50:37 +0100

    updating test1.php
    just adding a few more lines of code

--- test1.php
+++ test1.php
@@ -1,4 +1,9 @@
 <?php 
 //my new first git repo page with php
-echo "Working with new, exiting and pretty advanced features and tools here and now people...!";
+  echo "Hello World";
+  echo "Working with new, exiting and pretty advanced features and tools here and now people...!";
+//lets do more than echo a dummy text, lets try some operations
+  $var1 = 1;
+  $var2 = 2;
+  echo $sum = $var1 + $var2;
 ?>
